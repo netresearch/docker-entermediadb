0434acd2104fd377b0c7931dada2ec76c8775023
Norman Bestfleisch <norman.bestfleisch@netresearch.de>
2016-11-10 15:21:57 +0100

    Refactor docker entrypoint in order to persist data at container startup

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN sed -i "s/httpredir.debian.org/`curl -s -D - http://httpredir.debian.org/dem
     && gpg --quiet --armor --export 5C808C2B65558117 | apt-key add - \
     && wget --quiet  -O /etc/apt/trusted.gpg.d/entermediadb.gpg http://packages.entermediadb.org/repo/apt/entermediadb.gpg \
     && apt-get update \
-    && apt-get install -y --force-yes --no-install-recommends entermediadb \
+    && apt-get install -y --force-yes --no-install-recommends entermediadb rsync \
     && apt-get autoremove -y \
     && apt-get clean all \
     && rm -rf /var/lib/apt/lists/*
@@ -19,20 +19,7 @@ RUN sed -i 's/esac/foreground)\nsu $TOMCAT7_USER -c "$CATALINA_BASE\/bin\/catali
 
 RUN cp -rp "/usr/share/entermediadb/conf/ffmpeg" "/home/entermedia/.ffmpeg" \
     && mv /etc/ImageMagick-6/delegates.xml /etc/ImageMagick-6/delegates.old \
-    && cp -p "/usr/share/entermediadb/conf/im/delegates.xml" /etc/ImageMagick-6/ \
-    && bash -c "mkdir -p /opt/entermediadb/tomcat/{logs,temp}" \
-    && cp -rp "/usr/share/entermediadb/tomcat/conf" "/opt/entermediadb/tomcat" \
-    && cp -rp "/usr/share/entermediadb/tomcat/bin" "/opt/entermediadb/tomcat" \
-    && sed "s/%PORT%/8080/g" <"/usr/share/entermediadb/tomcat/conf/server.xml.template" >"/opt/entermediadb/tomcat/conf/server.xml" \
-    && sed "s|%ENDPOINT%|/opt/entermediadb|g" <"/usr/share/entermediadb/tomcat/bin/tomcat.template" >"/opt/entermediadb/tomcat/bin/tomcat" \
-    && echo "export CATALINA_BASE=\"/opt/entermediadb/tomcat\"" >> "/opt/entermediadb/tomcat/bin/setenv.sh" \
-    && echo "export JRE_HOME=\"${JAVA_HOME}\"" >> "/opt/entermediadb/tomcat/bin/setenv.sh" \
-    && chmod 755 "/opt/entermediadb/tomcat/bin/tomcat"
-    
-RUN cp -rp /usr/share/entermediadb/webapp /opt/entermediadb/ \
-    && mkdir -p /media \
-    && mv /opt/entermediadb/webapp/WEB-INF/data /media/data \
-    && ln -s /media/data /opt/entermediadb/webapp/WEB-INF/data
+    && cp -p "/usr/share/entermediadb/conf/im/delegates.xml" /etc/ImageMagick-6/
 
 EXPOSE 8080
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
-set -e
+set -xe
+EMDB_SOURCE=/usr/share/entermediadb
+EMDB_TARGET=/opt/entermediadb
+EMDB_WEBAPP=${EMDB_TARGET}/webapp
+EMDB_MEDIA=/media/data
 
-cp -rpn /usr/share/entermediadb/webapp/WEB-INF/data/* /media/data
+# application data
+if [[ ! -d "${EMDB_WEBAPP}/assets/emshare" ]]; then
+    mkdir -p "${EMDB_WEBAPP}"
+    cp -ar "${EMDB_SOURCE}/webapp" "${EMDB_TARGET}"
+fi
 
+if [[ ! -d "${EMDB_MEDIA}/system" ]]; then
+    mkdir -p "${EMDB_MEDIA}"
+    cp -arn "${EMDB_WEBAPP}/WEB-INF/data" "/media"
+    rm -rf "${EMDB_WEBAPP}/WEB-INF/data"
+    ln -s "${EMDB_MEDIA}" "${EMDB_WEBAPP}/WEB-INF/data"
+fi
+
+rsync -ar --delete --exclude '/WEB-INF/data' --exclude '/WEB-INF/elastic'  ${EMDB_SOURCE}/webapp/WEB-INF ${EMDB_WEBAPP}
+
+# tomcat data
+if [[ ! -d "${EMDB_TARGET}/tomcat/conf" ]]; then
+    mkdir -p ${EMDB_TARGET}/tomcat/{logs,temp}
+    cp -rp  ${EMDB_SOURCE}/tomcat/{bin,conf}  "${EMDB_TARGET}/tomcat"
+    sed "s/%PORT%/8080/g" < "${EMDB_SOURCE}/tomcat/conf/server.xml.template" > "${EMDB_TARGET}/tomcat/conf/server.xml"
+    sed "s|%ENDPOINT%|/opt/entermediadb|g" < "${EMDB_SOURCE}/tomcat/bin/tomcat.template" > "${EMDB_TARGET}/tomcat/bin/tomcat"
+    echo "export CATALINA_BASE=\"${EMDB_TARGET}/tomcat\"" >> "${EMDB_TARGET}/tomcat/bin/setenv.sh"
+    echo "export JRE_HOME=\"${JAVA_HOME}\"" >> "${EMDB_TARGET}/tomcat/bin/setenv.sh"
+    chmod 755 "${EMDB_TARGET}/tomcat/bin/tomcat"
+fi
+
+rsync -ar --delete \
+    --exclude '/tomcat/bin/setenv.sh' \
+    --exclude '/tomcat/bin/tomcat' \
+    --exclude '/tomcat/conf/server.xml'\
+    --exclude '/tomcat/conf/tomcat-users.xml' \
+    --exclude '/tomcat/logs/*' \
+    --exclude '/tomcat/conf/node.xml' \
+    $EMDB_SOURCE/tomcat $EMDB_TARGET/
+
+# apply overrides
 for d in /overrides/*/ ; do
     cp -r "$d"* /opt/entermediadb/webapp;
 done
 
+# set file ownerships
 chown -R entermedia. /opt/entermediadb
 chown -R entermedia. /media/data
-chown -R entermedia:entermedia /home/entermedia
+chown -R entermedia. /home/entermedia
 
 # Hoped this could fix https://github.com/entermedia-community/app-emshare/issues/7
 # but it doesn't
